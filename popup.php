6613a7b46236d90f7d13a40802cab139dcdc56e6
umutbulut091-cell <umutbulut091@gmail.com>
2026-06-30 23:11:19 +0530

    Update popup.php

--- popup.php
+++ popup.php
@@ -110,6 +110,7 @@ $encJs     = obf($js, $KEY);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
 </head>
 <body>
   <div id="app"></div>
